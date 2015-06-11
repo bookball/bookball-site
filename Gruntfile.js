6feba0c7344775cd8b9c8605bf4c2ac7b21076b9
Joao Beleza Freire <joao@bitbasement.com>
2015-06-11 09:41:52 +0100

    Hover color social icons

--- Gruntfile.js
+++ Gruntfile.js
@@ -107,6 +107,7 @@ module.exports = function(grunt) {
                 dest: "src/assets/icons"
             }],
             options: {
+              enhanceSVG: true
             }
         }
     },
